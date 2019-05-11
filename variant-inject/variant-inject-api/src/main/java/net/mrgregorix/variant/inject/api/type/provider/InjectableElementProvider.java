1cc110a976a9dad136d11af2ee98d08d73297112
MrGregorix <contact@mrgregorix.net>
2019-05-11 16:29:23 +0200

    Add missing docs

--- variant-inject/variant-inject-api/src/main/java/net/mrgregorix/variant/inject/api/type/provider/InjectableElementProvider.java
+++ variant-inject/variant-inject-api/src/main/java/net/mrgregorix/variant/inject/api/type/provider/InjectableElementProvider.java
@@ -5,10 +5,29 @@ import java.util.Collection;
 import net.mrgregorix.variant.inject.api.type.InjectableElement;
 import net.mrgregorix.variant.utils.annotation.CollectionMayBeImmutable;
 
+/**
+ * Provider for creating {@link InjectableElement} values for classes.
+ *
+ * @param <E> {@link InjectableElement} subtype that is created by this provider.
+ *
+ * @see InjectableElement
+ */
 public interface InjectableElementProvider <E extends InjectableElement>
 {
+    /**
+     * Returns the base type of {@link InjectableElement} that this provider is capable of creating.
+     *
+     * @return the base type of {@link InjectableElement} that this provider is capable of creating.
+     */
     Class<E> getType();
 
+    /**
+     * Returns a collection of {@link InjectableElement}s created from the given class.
+     *
+     * @param clazz clazz to created elements from.
+     *
+     * @return collection of {@link InjectableElement}
+     */
     @CollectionMayBeImmutable
     Collection<E> provide(Class<?> clazz);
 }
