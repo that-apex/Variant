b975073a1d7fdc14b2c72a786199b16cb3333178
MrGregorix <contact@mrgregorix.net>
2019-07-04 16:26:28 +0200

    Add Registry and its basic implementations

---
+++ variant-utils/src/main/java/net/mrgregorix/variant/utils/registry/CollectionBackedRegistry.java
@@ -0,0 +1,41 @@
+package net.mrgregorix.variant.utils.registry;
+
+import java.util.Collection;
+
+/**
+ * A {@link Registry} implementation that used a backing {@link Collection} to store the data
+ *
+ * @param <T> type of the stored objects
+ */
+public class CollectionBackedRegistry <T> implements Registry<T>
+{
+    private final Collection<T> backingCollection;
+
+    /**
+     * Creates a new CollectionBackedRegistry with the supplied backing collection
+     *
+     * @param backingCollection backed collection
+     */
+    public CollectionBackedRegistry(final Collection<T> backingCollection)
+    {
+        this.backingCollection = backingCollection;
+    }
+
+    @Override
+    public Collection<T> getRegisteredObjects()
+    {
+        return this.backingCollection;
+    }
+
+    @Override
+    public boolean register(final T object)
+    {
+        return this.backingCollection.add(object);
+    }
+
+    @Override
+    public boolean unregister(final T object)
+    {
+        return this.backingCollection.remove(object);
+    }
+}
---
+++ variant-utils/src/main/java/net/mrgregorix/variant/utils/registry/Registry.java
@@ -0,0 +1,39 @@
+package net.mrgregorix.variant.utils.registry;
+
+import java.util.Collection;
+
+import net.mrgregorix.variant.utils.annotation.CollectionMayBeImmutable;
+
+/**
+ * Represents a registry that can store objects and be used to register and unregister them.
+ *
+ * @param <T> type of the stored objects
+ */
+public interface Registry <T>
+{
+    /**
+     * Returns all the objects registered to this registry.
+     *
+     * @return all the objects registered to this registry
+     */
+    @CollectionMayBeImmutable
+    Collection<T> getRegisteredObjects();
+
+    /**
+     * Registers a new object to this registry
+     *
+     * @param object object to register
+     *
+     * @return true if the object was registered successfully
+     */
+    boolean register(T object);
+
+    /**
+     * Unregisters an object from this registry
+     *
+     * @param object object to unregister
+     *
+     * @return true if the object was unregistered successfully
+     */
+    boolean unregister(T object);
+}
